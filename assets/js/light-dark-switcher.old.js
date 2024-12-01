6ea4b5424fe9736cae65966bbfdf58a207c1224d
Twirrek <tarik_bensakhria@yahoo.fr>
2024-12-01 04:58:20 +0100

    Updated from Publii

--- assets/js/light-dark-switcher.old.js
+++ assets/js/light-dark-switcher.old.js
@@ -1,6 +1,5 @@
 // Encapsulation du code dans une fonction immédiatement invoquée
 (function() {
-	
   // Déclaration des variables
   const themeSwitcher = {
     buttons: document.querySelectorAll('.js-theme-switcher'),
@@ -8,44 +7,37 @@
     currentTheme: null
   };
 
-	document.documentElement.dataset.style = 'wipe';
-
   // Initialisation du thème
   function initTheme() {
-		
-    if (localStorage.getItem('theme-preference'))
+    if (localStorage.getItem('theme-preference')) {
       themeSwitcher.currentTheme = localStorage.getItem('theme-preference');
-    else 
-	  themeSwitcher.currentTheme = themeSwitcher.prefersDark.matches ? 'dark' : 'light';
-
-    SWITCH();
+    } else if (themeSwitcher.prefersDark.matches) {
+      themeSwitcher.currentTheme = 'dark';
+    } else {
+      themeSwitcher.currentTheme = 'light';
+    }
+    setTheme(themeSwitcher.currentTheme);
   }
 
   // Gestion du clic sur le bouton
-  themeSwitcher.buttons.forEach( button => button.addEventListener('click', () => {
+  themeSwitcher.buttons.forEach( button => button.addEventListener('click', function() {
     themeSwitcher.currentTheme = document.documentElement.getAttribute('data-theme-preference') === "dark" ? "light" : "dark";
-    TOGGLE_THEME();
+    setTheme(themeSwitcher.currentTheme);
   }));
 
   // Gestion du changement de préférence système
-  themeSwitcher.prefersDark.addEventListener('change', (event) => {
+  themeSwitcher.prefersDark.addEventListener('change', function(event) {
     themeSwitcher.currentTheme = event.matches ? 'dark' : 'light';
-    TOGGLE_THEME();
+    setTheme(themeSwitcher.currentTheme);
   });
 
   // Fonction pour définir le thème
-  function SWITCH() {
-	const theme = themeSwitcher.currentTheme;
+  function setTheme(theme) {
     const pressed = theme === 'dark' ? 'true' : 'false';
     document.documentElement.setAttribute('data-theme-preference', theme);
     localStorage.setItem('theme-preference', theme);
     themeSwitcher.buttons.forEach( button => button.setAttribute('aria-pressed', pressed));
   }
-
-	const TOGGLE_THEME = () => {
-	  if (!document.startViewTransition) SWITCH();
-	  else document.startViewTransition(SWITCH)
-	}
 
   // Initialisation
   initTheme();
